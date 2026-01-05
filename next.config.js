bdd1d42f526db2e88a8fb0156b8ea2e83a7d13be
ams-soonil <ksi@amass.co.kr>
2026-01-05 16:22:43 +0900

    셋팅수정

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/MinwonTalk' : '';
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
